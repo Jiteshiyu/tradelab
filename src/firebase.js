f8439662196c67b63bfeb13d653adabd3fcbfeb2
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2025-03-09 13:00:02 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  setPersistence,
-  browserLocalPersistence,
-} from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA2HtT08m4hBuoBUllfoSn5kHqTzwziPC4",
@@ -16,22 +11,8 @@ const firebaseConfig = {
   measurementId: "G-5TGL65W3TE",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Get the Auth instance
 const auth = getAuth(app);
-
-// Set persistence to local storage
-setPersistence(auth, browserLocalPersistence)
-  .then(() => {
-    console.log("Persistence set to browserLocalPersistence");
-  })
-  .catch((error) => {
-    console.error("Error setting persistence:", error);
-  });
-
-// Initialize GoogleAuthProvider
 const googleProvider = new GoogleAuthProvider();
 
 export { auth, googleProvider };
